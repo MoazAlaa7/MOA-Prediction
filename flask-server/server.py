e8e8a3f38dd7751935b1b727cfad6faaa267f093
MoazAlaa7 <moazalaa4727@gmail.com>
2024-04-25 18:33:03 +0200

    Added Endpoints & Handlers for uploading prediction data & downloading results

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, Response, make_response, jsonify
 from flask_cors import CORS
 import pandas as pd
 import numpy as np
@@ -8,12 +8,28 @@ from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.models import load_model
 from io import StringIO
 
-
 app = Flask(__name__)
-CORS(app)
+CORS(app)   
+
+encoderGene = None
+encoderCell = None
+# model = None
+sub = None 
+
+def load_models():
+    global encoderGene, encoderCell, sub #, model
+    if encoderGene is None:
+        encoderGene = load_model('C:/Users/Moaz/Desktop/New folder/link/encoder+xgboost_gene_features.h5')
+    if encoderCell is None:
+        encoderCell = load_model('C:/Users/Moaz/Desktop/New folder/link/encoder+xgboost_cell_features.h5')
+    # if model is None:
+    #     model = pickle.load(open('C:/Users/Moaz/Desktop/link/xgboost+autoencoder.pkl', 'rb'))
+    if sub is None:
+        sub = pd.read_csv('C:/Users/Moaz/Desktop/New folder/link/sample_submission.csv')
 
 def preprocessData(inputData):
-    #ENCODING THE CATEGORICAL inputData
+    load_models()
+
     le = LabelEncoder()
     inputData['cp_type'] = le.fit_transform(inputData['cp_type'])
     inputData['cp_time'] = le.fit_transform(inputData['cp_time'])
@@ -27,17 +43,13 @@ def preprocessData(inputData):
         if i.startswith('c-'):
             cell_features.append(i)
 
-    encoder = load_model('C:/Users/DELL/Downloads/Telegram Desktop/encoder+xgboost_gene_features.h5')
-    test_gene_features = encoder.predict(inputData[gene_features])
-
-    encoder = load_model('C:/Users/DELL/Downloads/Telegram Desktop/encoder+xgboost_cell_features.h5')
-    test_cell_features = encoder.predict(inputData[cell_features])
+    test_gene_features = encoderGene.predict(inputData[gene_features])
+    test_cell_features = encoderCell.predict(inputData[cell_features])
 
     x_1_test = np.hstack((inputData['cp_type'].values.reshape(-1,1), inputData['cp_time'].values.reshape(-1, 1), inputData['cp_dose'].values.reshape(-1, 1),
                         test_gene_features, test_cell_features))
 
     return x_1_test
-
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -54,47 +66,30 @@ def upload_file():
 
     new_data = preprocessData(data)
     
-    model = pickle.load(open('C:/Users/DELL/Downloads/Telegram Desktop/xgboost+autoencoder.pkl', 'rb'))
+    model = pickle.load(open('C:/Users/Moaz/Desktop/New folder/link/xgboost+autoencoder.pkl', 'rb'))
     prediction = model.predict_proba(new_data)
     prediction = np.array(prediction)[:, :, 1].T
 
-    print('FORMAT OF THE PREDICTED OUTPUT : ', type(prediction)) #<class 'scipy.sparse.lil.lil_matrix'>
+    print('FORMAT OF THE PREDICTED OUTPUT : ', type(prediction)) 
     print('DONE PREDICTION')
     end_time = time.time()
     print('TIME TAKEN TO PREDICT IS : {}'.format(end_time - start_time))
 
-    sub = pd.read_csv('C:/Users/DELL/Downloads/Telegram Desktop/sample_submission.csv')
     sub.iloc[:, 1:]  = prediction
 
-    # Download results
-    # response = make_response(sub.to_csv())
-    # response.headers["Content-Disposition"] = "attachment; filename=prediction.csv"
-    # return response
+    return jsonify({"file_ready": True})
 
-    # Show results on page
-    prediction_data = sub.to_dict(orient='records')
-    return jsonify(prediction_data)
+@app.route('/download', methods=['GET'])
+def download_file():
+    csv_output = StringIO()
+    sub.to_csv(csv_output, index=False)
+    csv_output.seek(0)
+
+    return Response(
+        csv_output.getvalue(),  
+        mimetype="text/csv",
+        headers={"Content-disposition": "attachment; filename=prediction.csv"}
+    )
 
 if __name__ == '__main__': 
     app.run(debug=True)
-# import csv
-# from flask import Flask, request, jsonify
-# from flask_cors import CORS
-
-# app = Flask(__name__)
-# CORS(app)
-
-# @app.route('/upload', methods=['POST'])
-# def upload_file():
-#     if request.method == 'POST':
-#         # Handle POST request
-        
-#         data = request.json.get('data')
-#         first_row = data[0] # Access the first row of the data
-#         print("First row of data:", first_row)
-#         return jsonify({'message': 'Data received successfully', 'first_row': first_row}), 200
-#     else:
-#         return jsonify({'error': 'Method Not Allowed'}), 405
-
-# if __name__ == '__main__': 
-#     app.run(debug=True)
